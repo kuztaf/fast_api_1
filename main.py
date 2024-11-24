df6e9c41c28b9efc841c22bc958836ee205601cd
Gustavo Cali <umbreon77@gmail.com>
2024-11-24 23:26:52 +0100

    adding models file and post method to create items

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from models import Item
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!!!"}
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
